0ca5c309d16b4629be604d38e7e69b35716e1644
Chaitanya <chaitanyachaitu157@gmail.com>
2026-08-15 17:42:18 +0530

    ansible

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-resource "aws_vpc" "Roboshop" {
-  cidr_block       = "10.0.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
   instance_tenancy = "default"
+  enable_dns_hostnames = true
 
-  tags = {
-    Name = "Roboshop"
-  }
+  tags = local.common-tags
 }
