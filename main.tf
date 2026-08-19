88061ad3a1636f45dd0ebd0fea4746b9e513b078
Chaitanya <chaitanyachaitu157@gmail.com>
2026-08-19 12:45:04 +0530

    ansible

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "aws_internet_gateway" "main" {
   tags = local.igw-final-tags
 }
 
+#public subnets
 resource "aws_subnet" "public" {
   count = length(var.public-subnet-cidrs)
   vpc_id     = aws_vpc.main.id
@@ -27,3 +28,33 @@ resource "aws_subnet" "public" {
      var.public-subnet-tags
   )
 }
+
+resource "aws_subnet" "private" {
+  count = length(var.private-subnet-cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private-subnet-cidrs[count.index]
+  availability_zone = local.zone-names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = merge(
+    local.common-tags,
+    {
+      Name = "${var.project}-${var.environment}-private-${local.zone-names[count.index]}"
+    },
+    var.private-subnet-tags
+  )
+}
+
+resource "aws_subnet" "data-base" {
+  count = length(var.data-base-subnet-cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.data-base-subnet-cidrs[count.index]
+  availability_zone = local.zone-names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = merge(
+    local.common-tags,
+    {
+      Name = "${var.project}-${var.environment}-data-base-${local.zone-names[count.index]}"
+    },
+    var.data-base-subnet-tags
